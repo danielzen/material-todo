378e41dcedbacbea25d9aef7fa8e4531dd3cbab5
Daniel Zen <daniel@zen.digital>
2017-03-21 21:20:44 -0500

    Dialog Only branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,52 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showDialog = false;
-  editingTodo: any = null;
-  fieldValue = '';
-  todoList: any = [];
-  okButtonText = 'Create task';
-
-  todoDialog(todo = null) {
-    this.okButtonText = 'Create task';
-    this.fieldValue = '';
-    this.editingTodo = todo;
-    if (todo) {
-      this.fieldValue = todo.title;
-      this.okButtonText = 'Edit task';
-    }
-    this.showDialog = true;
+  log(text) {
+    console.log(text);
   }
-
-  remove(index: number) {
-    this.todoList.splice(index, 1);
-  }
-
-  editTodo(title) {
-    this.editingTodo.title = title;
-  }
-
-  updateTodo(title) {
-    if (title) {
-      title = title.trim();
-      if (this.editingTodo) {
-        this.editTodo(title);
-      } else {
-        this.addTodo(title);
-      }
-    }
-    this.hideDialog();
-  }
-
-  addTodo(title) {
-    const todo = {title: title, completed: false};
-    this.todoList.push(todo);
-  }
-
-  hideDialog() {
-    this.showDialog = false;
-    this.editingTodo = null;
-    this.fieldValue = null; // make sure Input is new
-  }
-
 }
